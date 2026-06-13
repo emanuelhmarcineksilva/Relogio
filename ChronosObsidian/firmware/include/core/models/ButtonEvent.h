9ce0502239e543531f55174956fcacd2d333d207
emanuelhmarcineksilva <emanuel.henriquemarcinek.silva@escola.pr.gov.br>
2026-06-13 09:15:38 -0300

    Nova fase do projeto: Arquitetuna refeita, nesse momento estaremos usando o PlantFormIO para compilar e enviar o projeto para o ESP32

--- ChronosObsidian/firmware/include/core/models/ButtonEvent.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <Arduino.h>
-
-struct ButtonEvent {
-  int button_id;
-  unsigned long timestamp_ms;
-  uint8_t debounce_state;
-};
--- ChronosObsidian/firmware/src/main.cpp
+++
@@ -1,8 +0,0 @@
-// Ponto de entrada — pragma de otimização (FASE 2.5)
-#pragma GCC optimize("O2")
-
-#include "ChronosObsidian.h"
-
-// setup() e loop() estão em:
-//   src/sistema/boot/BootSequence.cpp
-//   src/sistema/MainLoop.cpp
